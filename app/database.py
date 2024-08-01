8ea5b584cc118a8b2da32c21248375369912c744
khlilsaif <saifkhlil@proton.me>
2024-08-01 17:47:37 +0100

    prot

--- app/database.py
+++ app/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
+
+load_dotenv()
 # Adjust the following PostgreSQL URL to match your database settings
 SQLALCHEMY_DATABASE_URL = os.getenv("db_url")
 
--- app/helpers/token.py
+++ app/helpers/token.py
@@ -4,6 +4,8 @@ from jose import JWTError, jwt
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 ACCESS_TOKEN_EXPIRE_DAYS = os.getenv("ACCESS_TOKEN_EXPIRE_DAYS")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
